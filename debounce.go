b2d97e2e0199f66be032572283db5429015677d3
Jaco Esterhuizen <jaco@iMac-de-Jaco.local>
2016-04-26 15:52:06 +0200

    Separate signal generation from rune IO.

--- debounce.go
+++ debounce.go
@@ -12,21 +12,49 @@ import (
 
 func main() {
 	d := flag.Int64("i", 300, "duration in milliseconds after last action, after which function executes")
-	loop(os.Stdin, os.Stdout, time.Duration(*d)*time.Millisecond)
+	Runes(os.Stdin, os.Stdout, time.Duration(*d)*time.Millisecond)
 }
 
+// Signals debounces the input signal, using duration d.  To stop listening, close the input channel; the output channel will be closed automatically.
+func Signals(d time.Duration) (chan<- struct{}, <-chan struct{}) {
+	in, out := make(chan struct{}), make(chan struct{})
+	t := time.NewTimer(time.Hour)
+	t.Stop()
 
+	exit := make(chan struct{})
 
-func loop(in io.Reader, out io.Writer, d time.Duration) {
-	var (
-		t = time.NewTimer(d)
-		c rune
-	)
+	go func() {
+		defer close(out)
+		
+		for x := false; !x; {
+			select {
+			case <-t.C:
+				out <- struct{}{}
+			case <-exit:
+				x = true
+			}
+		}
+	}()
+
+	go func() {
+		for range in {
+			t.Reset(d)
+		}
+		exit <- struct{}{}
+	}()
+	
+	return in, out
+}
+
+// Runes debounces runes read from in. To stop debouncing, call the returned function.
+func Runes(in io.Reader, out io.Writer, d time.Duration) func() {
+	var c rune
+
+	cin, cout := Signals(d)
 
 	go func() {
 		p := make([]byte, 4)
-		for {
-			<-t.C
+		for range cout {
 			n := utf8.EncodeRune(p, c)
 			out.Write(p[:n])
 		}
@@ -35,12 +63,17 @@ func loop(in io.Reader, out io.Writer, d time.Duration) {
 	r := bufio.NewReader(in)
 	var err error
 
-	for {
-		if c, _, err = r.ReadRune(); err != nil {
-			printErr(err)
+	go func() {
+		for {
+			if c, _, err = r.ReadRune(); err != nil {
+				printErr(err)
+			}
+			cin <- struct{}{}
 		}
+	}()
 	
-		t.Reset(d)
+	return func() {
+		close(cin)
 	}
 }
 
--- debounce_test.go
+++ debounce_test.go
@@ -7,14 +7,37 @@ import (
 	"time"
 )
 
-func TestDebounce(t *testing.T) {
+func TestSignals(t *testing.T) {
+	d := 300 * time.Millisecond
+	in, out := Signals(d)
+
+	go func() {
+		for i := 0; i < 3; i++ {
+			in <- struct{}{}
+		}
+		time.Sleep(d * 2)
+		close(in)
+	}()
+
+	got := 0
+
+	for range out {
+		got++
+	}
+
+	if got != 1 {
+		t.Errorf("Wanted 1, but got %d", got)
+	}
+}
+
+func TestRunes(t *testing.T) {
 
 	d := 300 * time.Millisecond
 
 	pIn, in := io.Pipe()
 	out := new(bytes.Buffer)
 
-	go loop(pIn, out, d)
+	x := Runes(pIn, out, d)
 
 	_, err := in.Write([]byte{'a', 'b', 'c'})
 	if err != nil {
@@ -27,6 +50,9 @@ func TestDebounce(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+
+	x()
+
 	if got != 'c' {
 		t.Errorf("Wanted 'c', but got '%c'", got)
 	}
