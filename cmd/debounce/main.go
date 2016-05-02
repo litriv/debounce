309327d91dbf98f5518948924be9c4f6b9b7029d
Jaco Esterhuizen <jaco@litriv.com>
2016-05-02 15:04:12 +0200

    *.go: change Runes for IO to generalise

--- cmd/debounce/main.go
+++ cmd/debounce/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"flag"
-	"os"
-	"time"
 	"litriv.com/debounce"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
 	d := flag.Int64("i", 300, "duration in milliseconds after last action, after which function executes")
-	debounce.Runes(os.Stdin, os.Stdout, time.Duration(*d)*time.Millisecond)
+	s := flag.String("s", "lines", "'lines' to split on lines, 'runes' to split on runes")
+	var sf bufio.SplitFunc
+	switch *s {
+	case "runes":
+		sf = bufio.ScanRunes
+	default:
+		sf = bufio.ScanLines
+	}
+	debounce.IO(os.Stdin, os.Stdout, time.Duration(*d)*time.Millisecond, sf)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
 }
